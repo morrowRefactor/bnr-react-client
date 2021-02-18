3d4bd4d43731d31dda5d8ec81d3b5f3bde82d24d
Kurt Morrow <kurt.morrow@gmail.com>
2021-02-18 10:26:18 -0800

    updated About page and Contact Us form styling

--- src/About/About.js
+++ src/About/About.js
@@ -16,15 +16,15 @@ class About extends Component {
                 </div>
                 <section className='About_info'>
                     <div className='aboutInfoMission'>
-                        <h3>My Mission</h3>
+                        <h3 className='aboutInfoHeader'>My Mission</h3>
                         <p>To take a news topic and try to find the underlying effects that might not have been considered.  I will be drinking a beer to make the discussion more relaxed and comical.</p>
                     </div>
                     <div className='aboutInfoCompany'>
-                        <h3>My Company</h3>
+                        <h3 className='aboutInfoHeader'>My Company</h3>
                         <p>All the shows produced here were mainly done at the CreaTV studios in San Jose, CA.  This is a public access studio so all the actors, including myself, were not paid.  It is truly working for the joy of it...and of course beer.</p>
                     </div>
                     <div className='aboutInfoDisclaimer'>
-                        <h3>My Disclaimer</h3>
+                        <h3 className='aboutInfoHeader'>My Disclaimer</h3>
                         <p>All the topics I talk about are either my opinion or obvious parody.  I do not claim to be an expert on anything and that includes beer drinking.  Any discussion or argument that uses my news as its basis is doomed to failure for which I am not responsible for.</p>
                     </div>
                 </section>
