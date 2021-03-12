50a3fbd0e82bd00356b80152e68730bae1526a16
Kurt Morrow <kurt.morrow@gmail.com>
2021-03-12 12:39:15 -0800

    updated server for production

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000',
+    API_ENDPOINT: 'https://peaceful-wave-86345.herokuapp.com',
     TOKEN_KEY: 'bnr-client-auth-token'
 }
-
-/*https://peaceful-wave-86345.herokuapp.com*/
